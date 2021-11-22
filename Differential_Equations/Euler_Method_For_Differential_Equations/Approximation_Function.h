8c6b82a0efd7779a488fd0af45461b51a36d7469
George M <giorgos_matlis@hotmail.com>
2021-11-22 20:59:44 +0200

    Create Approximation_Function.h

---
+++ Differential_Equations/Euler_Method_For_Differential_Equations/Approximation_Function.h
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+// Approximation Function
+double approximation_function(double t, double y){
+    //return (-1.0*(2.0*y) + 3.0 + t);
+    //return (2.0*y) - (4.0*t) + 2.0;
+    //return 2.0 - exp(-4.0*t) - (2.0*y);
+}
