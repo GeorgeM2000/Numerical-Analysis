92ca5b555f188a58f791f783ad6a1d0385a76eee
George M <giorgos_matlis@hotmail.com>
2021-11-22 20:59:40 +0200

    Create Exact_Function.h

---
+++ Differential_Equations/Euler_Method_For_Differential_Equations/Exact_Function.h
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+// Exact Function
+double exact_function(double t){
+    //return ((t / 2.0) + 1.25 - (0.25 / exp(2.0*t)));
+    //return 2.0*(exp(2.0*t) + t);
+   //return 1.0 + (1.0/2.0)*exp(-4.0*t) - (1.0/2.0)*exp(-2.0*t);
+}
