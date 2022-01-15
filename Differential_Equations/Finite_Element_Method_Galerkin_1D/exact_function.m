ff703f0d0e698cd1a48d1fe499a881387fbaacfc
George M <giorgos_matlis@hotmail.com>
2022-01-15 17:28:58 +0200

    Update exact_function.m

--- Differential_Equations/Finite_Element_Method_Galerkin_1D/exact_function.m
+++ Differential_Equations/Finite_Element_Method_Galerkin_1D/exact_function.m
@@ -1,28 +1,5 @@
-## Copyright (C) 2021 giorg
-##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-## -*- texinfo -*-
-## @deftypefn {} {@var{retval} =} exact_function (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: giorg <giorg@DESKTOP-UB8J40I>
-## Created: 2021-12-29
-
 function y = exact_function (x)
-  y = (x*(1-x))/2;
+  %y = (x*(1-x))/2;
+  y = x^3 - 6*x + ((5*sin(x)) / sin(1));
   
 endfunction
