5a116ff05bd8380a47ae662ee3db2640f8a8245e
George M <giorgos_matlis@hotmail.com>
2022-01-15 17:29:05 +0200

    Update global_stiffness_matrix.m

--- Differential_Equations/Finite_Element_Method_Galerkin_1D/global_stiffness_matrix.m
+++ Differential_Equations/Finite_Element_Method_Galerkin_1D/global_stiffness_matrix.m
@@ -22,12 +22,23 @@
 ## Author: giorg <giorg@DESKTOP-UB8J40I>
 ## Created: 2021-12-29
 
-function [A, F, U] = global_stiffness_matrix (x)
+function [A, F, U] = global_stiffness_matrix (x, y0, ye, h=1, px=1, qx=1)
+  % Calculate h for variable sized intervals
   M = length(x);
+  
+  %{
+  For variable sized mesh
+  
   for i=1:M-1,
     h(i) = x(i+1)-x(i);
   end
+  %}
+  
   A = sparse(M,M); F=zeros(M,1); % Initialization
+  
+  %{  
+  %------ For 1D models: -u''(x) = f(x)
+  
   A(1,1) = 1; F(1)=0;
   A(M,M) = 1; F(M)=0;
   A(2,2) = 1/h(1); F(2) = load_vector_dintegral(x(1),x(2), 1);
@@ -46,7 +57,48 @@ function [A, F, U] = global_stiffness_matrix (x)
   
   A(M-1,M-1) = A(M-1,M-1) + 1/h(M-1);
   F(M-1) = F(M-1) + load_vector_dintegral(x(M-1),x(M), 2);
-  U = A\F; % Solve the linear system of equations
+  %}
+  
+
+  %------- For 1D models: p(x)-u''(x) + q(x)u(x) = f(x)
+  
+  % Global stiffness matrix
+  A(1,1) = 1; A(M, M) = 1;
+  for i=2:M-1
+    for j=1:M
+      if i == j    
+        A(i,j) = px*(2/h) + qx*((2/3)*h);       
+      elseif i == j-1        
+        A(i,j) = px*(-(1/h)) + qx*(h/6);        
+      elseif i == j+1       
+        A(i,j) = px*(-(1/h)) + qx*(h/6);        
+      else
+        A(i,j) = 0;
+      endif
+    endfor
+  endfor
+  
+  
+  % Global load vector
+  # BC apply u(0) = 0
+  F(1) = y0;
+  
+  # BC apply u(1) = 0
+  F(M) = ye;
+  
+  F(2) = load_vector_dintegral(x(1),x(2), 1);
+  for i=2:M-2
+    % Local load vector 
+    F(i) = F(i) + load_vector_dintegral(x(i),x(i+1), 2);
+    F(i+1) = F(i+1) + load_vector_dintegral(x(i),x(i+1), 1);
+  endfor
+  F(M-1) = F(M-1) + load_vector_dintegral(x(M-1),x(M), 2);
+  
+  
+
+  
+  %U = A\F; % Solve the linear system of equations
+  U = gauss_elimination(A, F);
   return
 
 endfunction
